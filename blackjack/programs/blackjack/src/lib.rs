eb55a3d5dc13b34089868b6657ae2c994807dc5b
Kristofer Peterson <svenski123@users.noreply.github.com>
2023-10-04 21:01:09 +0200

    access slot hash sysvar by accessing account data directly add more checks to test cases

--- blackjack/programs/blackjack/src/lib.rs
+++ blackjack/programs/blackjack/src/lib.rs
@@ -1,29 +1,26 @@
 use anchor_lang::prelude::*;
-use anchor_lang::__private::bytemuck;
-use anchor_lang::solana_program::hash;
+use anchor_lang::solana_program::{hash,slot_hashes,sysvar,clock::Slot};
 use core::mem;
 
 declare_id!("11111111111111111111111111111111");
 
-fn get_rand_u8s(clock: &Clock, slot_hashes: &SlotHashes) -> [u8; 32] {
+fn get_rand_u8s(clock: &Clock, slot: u64, hash: [u8; 32]) -> [u8; 32] {
     const _: () = assert!(mem::size_of::<Clock>() == 40);
     const _: () = assert!(mem::size_of::<u64>() == 8);
     const _: () = assert!(mem::size_of::<hash::Hash>() == 32);
 
     let mut buf = Vec::<u8>::with_capacity(40 + 8 + 32);
 
-    buf.extend_from_slice(bytemuck::bytes_of(&clock.slot));
-    buf.extend_from_slice(bytemuck::bytes_of(&clock.epoch_start_timestamp));
-    buf.extend_from_slice(bytemuck::bytes_of(&clock.epoch));
-    buf.extend_from_slice(bytemuck::bytes_of(&clock.leader_schedule_epoch));
-    buf.extend_from_slice(bytemuck::bytes_of(&clock.unix_timestamp));
+    buf.extend_from_slice(&clock.slot.to_ne_bytes());
+    buf.extend_from_slice(&clock.epoch_start_timestamp.to_ne_bytes());
+    buf.extend_from_slice(&clock.epoch.to_ne_bytes());
+    buf.extend_from_slice(&clock.leader_schedule_epoch.to_ne_bytes());
+    buf.extend_from_slice(&clock.unix_timestamp.to_ne_bytes());
 
-    let (slot, hash) : &(u64, hash::Hash) = slot_hashes.first().unwrap();
-    buf.extend_from_slice(bytemuck::bytes_of(slot));
-    buf.extend_from_slice(hash.as_ref());
+    buf.extend_from_slice(&slot.to_ne_bytes());
+    buf.extend_from_slice(&hash);
 
-//    *bytemuck::from_bytes::<u64>(&hash::hash(&buf).as_ref()[0..mem::size_of::<u64>()])
-    *bytemuck::from_bytes(&hash::hash(&buf).as_ref())
+    hash::hash(&buf).to_bytes()
 }
 
 fn select_card(game: &mut Game, mut seed: u8) -> Option<u8> {
@@ -75,6 +72,24 @@ pub enum BlackJackError {
     GameOverDealerDone,
 }
 
+
+fn get_slot_hash_from_sysvar(slot_hashes: &UncheckedAccount) -> (u64, [u8; 32]) {
+    #[repr(C)]
+    struct SlotHashesData {
+	size: u64,
+	entries: [(Slot, [u8; 32]); slot_hashes::MAX_ENTRIES],
+    }
+
+    let slot_hashes = slot_hashes.data.borrow();
+    let slot_hashes = slot_hashes.as_ptr();
+    let slot_hashes = slot_hashes as *const SlotHashesData;
+    let slot_hashes = unsafe { &*slot_hashes };
+
+    //msg!("gshfs: size:{} slot:{} hash:{:?}", slot_hashes.size, slot_hashes.entries[0].0, slot_hashes.entries[0].1);
+
+    slot_hashes.entries[0]
+}
+
 #[program]
 pub mod blackjack {
     use super::*;
@@ -94,7 +109,9 @@ pub mod blackjack {
 	for i in 0..52 {
 	    game.deck[i] = num_decks
 	}
-	let seeds: [u8; 32] = get_rand_u8s(&Clock::get()?, &ctx.accounts.slot_hashes);
+
+	let (slot, hash) = get_slot_hash_from_sysvar(&ctx.accounts.slot_hashes);
+	let seeds: [u8; 32] = get_rand_u8s(&Clock::get()?, slot, hash);
 	game.dealer_hand[0] = select_card(game, seeds[0]).unwrap();
 	game.player_hand[0] = select_card(game, seeds[1]).unwrap();
 	game.player_hand[1] = select_card(game, seeds[2]).unwrap();
@@ -109,7 +126,8 @@ pub mod blackjack {
 	let (_dealer_hi, dealer_lo) = calc_hi_lo_score(&game.dealer_hand[0..game.dealer_hand_size as usize]);
 	require!(player_hi <= 21, BlackJackError::GameOverPlayerBust);
 	require!(dealer_lo <= 16, BlackJackError::GameOverDealerDone); // dealer hits on soft ace
-	let seeds: [u8; 32] = get_rand_u8s(&Clock::get()?, &ctx.accounts.slot_hashes);
+	let (slot, hash) = get_slot_hash_from_sysvar(&ctx.accounts.slot_hashes);
+	let seeds: [u8; 32] = get_rand_u8s(&Clock::get()?, slot, hash);
 	let i = game.player_hand_size as usize;
 	game.player_hand[i] = select_card(game, seeds[0]).unwrap();
 	game.player_hand_size += 1;
@@ -122,7 +140,8 @@ pub mod blackjack {
 	let (_dealer_hi, dealer_lo) = calc_hi_lo_score(&game.dealer_hand[0..game.dealer_hand_size as usize]);
 	require!(player_hi <= 21, BlackJackError::GameOverPlayerBust);
 	require!(dealer_lo <= 16, BlackJackError::GameOverDealerDone); // dealer hits on soft ace
-	let seeds: [u8; 32] = get_rand_u8s(&Clock::get()?, &ctx.accounts.slot_hashes);
+	let (slot, hash) = get_slot_hash_from_sysvar(&ctx.accounts.slot_hashes);
+	let seeds: [u8; 32] = get_rand_u8s(&Clock::get()?, slot, hash);
 	let mut i = game.dealer_hand_size as usize;
 	for seed in seeds {
 	    game.dealer_hand[i] = select_card(game, seed).unwrap();
@@ -162,7 +181,9 @@ pub struct Deal<'info> {
     #[account(mut, has_one = player)]
     pub game: Account<'info, Game>,
     pub player: Signer<'info>,
-    pub slot_hashes: Sysvar<'info, SlotHashes>,
+    #[account(address = sysvar::slot_hashes::id())]
+    /// CHECK:
+    pub slot_hashes: UncheckedAccount<'info>,
 }
 
 #[derive(Accounts)]
@@ -170,7 +191,9 @@ pub struct Hit<'info> {
     #[account(mut, has_one = player)]
     pub game: Account<'info, Game>,
     pub player: Signer<'info>,
-    pub slot_hashes: Sysvar<'info, SlotHashes>,
+    #[account(address = sysvar::slot_hashes::id())]
+    /// CHECK:
+    pub slot_hashes: UncheckedAccount<'info>,
 }
 
 #[derive(Accounts)]
@@ -178,7 +201,9 @@ pub struct Stand<'info> {
     #[account(mut, has_one = player)]
     pub game: Account<'info, Game>,
     pub player: Signer<'info>,
-    pub slot_hashes: Sysvar<'info, SlotHashes>,
+    #[account(address = sysvar::slot_hashes::id())]
+    /// CHECK:
+    pub slot_hashes: UncheckedAccount<'info>,
 }
 
 #[derive(Accounts)]
